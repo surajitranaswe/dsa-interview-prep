ebd0cc68224e0f09b28c914115ad415e33b76381
Surajit Rana <surajitrana.swe@gmail.com>
2026-08-05 00:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (7.30%) - LeetSync

--- Grind75/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ Grind75/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,12 +10,36 @@
  */
 class Solution {
     /**
-     * Approach I : Using Two Pointers Approach
+     * Approach II : Using Recursion Approach
+     *
+     * TC : O(m + n)
+     * SC : O(m + n)
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Base Case
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        // Recursion Calls
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Two Pointers) Approach
      *
      * TC : O(m) + O(n) ~ O(m + n)
      * SC : O(1)
      */
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoListsTwoPointers(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
         ListNode p = list1; // pointer at the start of ListNode 'list1'
