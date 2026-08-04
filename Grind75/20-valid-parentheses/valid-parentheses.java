b81f97bb593a095cf3d7a6fed38f631a5fa81be5
Surajit Rana <surajitrana.swe@gmail.com>
2026-08-05 00:08:10 +0530

    Time: 3 ms (86.06%) | Memory: 43.4 MB (26.11%) - LeetSync

---
+++ Grind75/20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,60 @@
+class Solution {
+    /**
+     * Approach : Using Stack + String Simulation Approach
+     *
+     * TC : O(n)
+     * SC : O(n)
+     */
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack<>(); // SC : O(n)
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            char ch = s.charAt(i);    // TC : O(1)
+            if (isOpenBracket(ch)) {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    /**
+                     * if close bracket appears before 
+                     * any open brackets then String 's'
+                     * will always be invalid
+                     */
+                    return false;
+                }
+                char last = st.peek();
+                if (last == getOpenBracketForClosed(ch)) { // TC : O(1)
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
+    /**
+     * Using String Enumeration Approach
+     *
+     * TC : O(1)
+     * SC : O(1)
+     */
+    private boolean isOpenBracket(char ch) {
+        return ch == '(' || ch == '{' || ch == '[';
+    }
+
+    /**
+     * Using String Enumeration Approach
+     *
+     * TC : O(1)
+     * SC : O(1)
+     */
+    private char getOpenBracketForClosed(char ch) {
+        if (ch == ')') {
+            return '(';
+        } else if (ch == '}') {
+            return '{';
+        } else {
+            return '[';
+        }
+    }
+}
